1c1384eeaf2a96e14d6c6783ed8a8d4afbfb40b4
Debashish Pramanik <Debashish0610@gmail.com>
2020-08-10 00:36:02 +0600

    Added comments section in blog details page with approval process

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approve_comment=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approve_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
